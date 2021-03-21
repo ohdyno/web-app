ee31cb65d98a3aaef7fc2926a65cd154ecb8f005
Xing Zhou <xing@xingzhou.me>
2021-03-21 11:05:28 -0400

    feat: scissors vs rock results in player 2 wins

--- rps/index.js
+++ rps/index.js
@@ -1,5 +1,10 @@
 module.exports = class {
     playRound(player1Choice, player2Choice, ui) {
-        ui.player1Wins();
+        if (player1Choice === 'rock') {
+            ui.player1Wins();
+            return;
+        }
+
+        ui.player2Wins();
     }
 }
--- rps/rps.test.js
+++ rps/rps.test.js
@@ -9,3 +9,13 @@ test('rock vs scissors results in player 1 wins', () => {
 
     expect(uiSpy.player1Wins).toBeCalled();
 })
+
+test('scissors vs rock results in player 2 wins', () => {
+    const uiSpy = {
+        player2Wins: jest.fn()
+    };
+
+    new Game().playRound('scissors', 'rock', uiSpy)
+
+    expect(uiSpy.player2Wins).toBeCalled();
+})
