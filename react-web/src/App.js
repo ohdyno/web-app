bfee83dc19df8e9e18c85703cb89d0b17c6e6806
Xing Zhou <xing@xingzhou.me>
2021-03-22 22:37:44 -0400

    feat: display player 1 wins and player 2 wins

--- react-web/src/App.js
+++ react-web/src/App.js
@@ -1,7 +1,23 @@
-function App() {
-  return (
-      <p>player 2 wins</p>
-  );
+import React, {useState, useEffect} from 'react';
+
+function App({game}) {
+    let [result, setResult] = useState("");
+
+    useEffect(() => {
+        game.playRound('foo', 'bar', {
+            player2Wins() {
+                setResult("player 2 wins");
+            },
+
+            player1Wins() {
+                setResult("player 1 wins");
+            }
+        })
+    })
+
+    return (
+        <p>{result}</p>
+    );
 }
 
 export default App;
--- react-web/src/App.test.js
+++ react-web/src/App.test.js
@@ -12,3 +12,15 @@ test('when player 2 wins, display player 2 wins', () => {
 
     expect(screen.getByText(/player 2 wins/i)).toBeInTheDocument();
 });
+
+test('when player 1 wins, display player 1 wins', () => {
+    let player1WinStub = {
+        playRound(player1Choice, player2Choice, ui) {
+            ui.player1Wins();
+        }
+    };
+
+    render(<App game={player1WinStub}/>);
+
+    expect(screen.getByText(/player 1 wins/i)).toBeInTheDocument();
+});
