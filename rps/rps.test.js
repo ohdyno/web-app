3aff5c5d56f994e790d4a5c418f91d2723355a2a
Xing Zhou <xing@xingzhou.me>
2021-03-24 00:17:08 -0400

    refactor: reduce duplicate setup in rps tests

--- rps/rps.test.js
+++ rps/rps.test.js
@@ -1,44 +1,46 @@
 const Game = require('./index');
 
+const game = new Game();
+
 describe('player 1 wins scenarios', () => {
-    test('rock vs scissors', () => {
-        const uiSpy = {
+    let uiSpy;
+
+    beforeEach(() => {
+        uiSpy = {
             player1Wins: jest.fn()
         };
+    })
 
-        new Game().playRound('rock', 'scissors', uiSpy)
+    test('rock vs scissors', () => {
+        game.playRound('rock', 'scissors', uiSpy)
 
         expect(uiSpy.player1Wins).toBeCalled();
     })
 
     test('scissors vs paper', () => {
-        const uiSpy = {
-            player1Wins: jest.fn()
-        };
-
-        new Game().playRound('scissors', 'paper', uiSpy)
+        game.playRound('scissors', 'paper', uiSpy)
 
         expect(uiSpy.player1Wins).toBeCalled();
     })
 })
 
 describe('player 2 wins scenarios', () => {
-    test('scissors vs rock', () => {
-        const uiSpy = {
+    let uiSpy;
+
+    beforeEach(() => {
+        uiSpy = {
             player2Wins: jest.fn()
         };
+    })
 
-        new Game().playRound('scissors', 'rock', uiSpy)
+    test('scissors vs rock', () => {
+        game.playRound('scissors', 'rock', uiSpy)
 
         expect(uiSpy.player2Wins).toBeCalled();
     })
 
     test('rock vs paper', () => {
-        const uiSpy = {
-            player2Wins: jest.fn()
-        };
-
-        new Game().playRound('rock', 'paper', uiSpy)
+        game.playRound('rock', 'paper', uiSpy)
 
         expect(uiSpy.player2Wins).toBeCalled();
     })
