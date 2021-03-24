fe06895d6e5cc6a5d22b915e03201c974b47e8a5
Xing Zhou <xing@xingzhou.me>
2021-03-24 00:12:30 -0400

    refactor: extract player 1 wins method in rps

--- rps/index.js
+++ rps/index.js
@@ -1,11 +1,15 @@
 module.exports = class {
     playRound(player1Choice, player2Choice, ui) {
-        if (player1Choice === 'rock' ||
-            player1Choice === 'scissors' && player2Choice === 'paper') {
+        if (this.player1Wins(player1Choice, player2Choice)) {
             ui.player1Wins();
             return;
         }
 
         ui.player2Wins();
+    }
+
+    player1Wins(player1Choice, player2Choice) {
+        return player1Choice === 'rock' ||
+            player1Choice === 'scissors' && player2Choice === 'paper';
     }
 }
